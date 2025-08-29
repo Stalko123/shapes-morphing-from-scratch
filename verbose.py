b5ce6295a8cc48fb120c67941fc1d2bb090bb44a
Stalko123 <oussamaakar@gmail.com>
2025-08-30 01:41:14 +0200

    mock test for pr-metrics

---
+++ verbose.py
@@ -0,0 +1,27 @@
+def mathematical_functions(x):
+    def square(x):
+        return x ** 2
+
+    def cube(x):
+        return x ** 3
+
+    def square_root(x):
+        return x ** 0.5
+
+    def factorial(n):
+        if n < 0:
+            return None
+        elif n == 0:
+            return 1
+        else:
+            result = 1
+            for i in range(1, n + 1):
+                result *= i
+            return result
+
+    return {
+        'square': square(x),
+        'cube': cube(x),
+        'square_root': square_root(x),
+        'factorial': factorial(int(x))  # Factorial only for non-negative integers
+    }
