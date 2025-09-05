d6ac94c4a87416d9ec2aecf922431abfc8d605b4
Stalko123 <oussamaakar@gmail.com>
2025-09-05 15:34:53 +0200

    fixed import problems to adapt the training to the new code structure after refactoring

--- utils/parsing/args.py
+++ utils/parsing/args.py
@@ -1,7 +1,7 @@
-from argparser import args_parsed
+from .argparser import args_parsed
 from DDPM.denoisers.denoisermlp.denoisermlp import DenoiserMLP
 from DDPM.denoisers.denoiserunet.denoiserunet import DenoiserUNet
-from loaders.dataloader import DataLoader
+from loaders.dataloader import Dataloader
 import torch
 
 class Args:
@@ -12,79 +12,77 @@ class Args:
         # ---------------------------
         # General / run control
         # ---------------------------
-        self.t_max: int = args.t_max
-        self.n_epochs: int = args.n_epochs
-        self.verbose: bool = args.verbose
-        self.model_name: str = args.model  
+        self.t_max: int = args_parsed.t_max
+        self.n_epochs: int = args_parsed.n_epochs
+        self.verbose: bool = args_parsed.verbose
+        self.model_name: str = args_parsed.model  
 
         # ---------------------------
         # Data
         # ---------------------------
-        self.dataset_name: str = args.dataset
-        self.batch_size: int = args.batch_size
-        self.n_workers: int = args.n_workers
-        self.dataset = DataLoader(self.dataset_name, self.batch_size, self.n_workers, self.n_epochs)
+        self.dataset_name: str = args_parsed.dataset
+        self.batch_size: int = args_parsed.batch_size
+        self.n_workers: int = args_parsed.n_workers
+        self.dataset = Dataloader(self.dataset_name, self.batch_size, self.n_workers, self.n_epochs)
         self.image_shape = self.dataset.image_shape  # (C, H, W)
 
         # ---------------------------
         # Diffusion schedule / MC
         # ---------------------------
-        self.num_trials: int = args.num_trials
-        self.alpha_min: float = args.alpha_min
-        self.alpha_max: float = args.alpha_max
-        self.alpha_interp: str = args.alpha_interp
+        self.num_trials: int = args_parsed.num_trials
+        self.alpha_min: float = args_parsed.alpha_min
+        self.alpha_max: float = args_parsed.alpha_max
+        self.alpha_interp: str = args_parsed.alpha_interp
 
         # precompute alphas 
         if self.alpha_interp == "linear":
             self.alphas = torch.linspace(self.alpha_max, self.alpha_min, self.t_max)
         else:
-            raise ValueError(f"Argument error : interpolation method {args.alpha_interp} not implemented")
+            raise ValueError(f"Argument error : interpolation method {args_parsed.alpha_interp} not implemented")
 
         # ---------------------------
         # Training hyperparameters
         # ---------------------------
-        self.learning_rate: float = args.learning_rate
-        self.dropout: float = args.dropout
-        self.optimizer_name = args.optimizer
-        if self.optimizer_name.lower() == 'adam':
-            self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
+        self.learning_rate: float = args_parsed.learning_rate
+        self.dropout: float = args_parsed.dropout
+        self.optimizer_name = args_parsed.optimizer
 
         # ---------------------------
         # Model-shared knobs
         # ---------------------------
-        self.time_dim: int = args.time_dim
-        self.activation: str = args.activation
-        self.norm: str = args.norm
-        self.init_scheme: str = args.init_scheme
-        self.hidden_sizes = args.hidden_sizes
+        self.time_dim: int = args_parsed.time_dim
+        self.activation: str = args_parsed.activation
+        self.norm: str = args_parsed.norm
+        self.init_scheme: str = args_parsed.init_scheme
+        self.hidden_sizes = args_parsed.hidden_sizes
 
         # ---------------------------
         # U-Net / CNN-specific
         # ---------------------------
-        self.base_channels: int = args.base_channels
-        self.channel_mults = args.channel_mults              # tuple[int,...]
-        self.num_res_blocks = args.num_res_blocks            # int
-        self.upsample: str = args.upsample                   # "convtranspose" | "nearest_conv"
-        self.groups: int = args.groups                       # GroupNorm groups
-        self.num_res_blocks_in_bottleneck: int = args.num_res_blocks_in_bottleneck
-        self.kernel_size: int = args.kernel_size
-        self.downsample: str = args.downsample               # "stride" | "pool" | "avgpool"
-        self.time_hidden: int = args.time_hidden             # time MLP hidden
+        self.base_channels: int = args_parsed.base_channels
+        self.channel_mults = args_parsed.channel_mults              # tuple[int,...]
+        self.num_res_blocks = args_parsed.num_res_blocks            # int
+        self.upsample: str = args_parsed.upsample                   # "convtranspose" | "nearest_conv"
+        self.groups: int = args_parsed.groups                       # GroupNorm groups
+        self.num_res_blocks_in_bottleneck: int = args_parsed.num_res_blocks_in_bottleneck
+        self.kernel_size: int = args_parsed.kernel_size
+        self.downsample: str = args_parsed.downsample               # "stride" | "pool" | "avgpool"
+        self.time_hidden: int = args_parsed.time_hidden             # time MLP hidden
 
         # ---------------------------
         # Logging / checkpoints / outputs
         # ---------------------------
-        self.log_dir: str = args.log_dir
-        self.checkpoint_dir: str = args.checkpoint_dir
-        self.save_frequency: int = args.save_frequency
-        self.output_dir: str = args.output_dir
-        self.fps: int = args.fps
-        self.path_to_weights = args.path_to_weights
+        self.log_dir: str = args_parsed.log_dir
+        self.checkpoint_dir: str = args_parsed.checkpoint_dir
+        self.save_frequency: int = args_parsed.save_frequency
+        self.output_dir: str = args_parsed.output_dir
+        self.fps: int = args_parsed.fps
+        self.path_to_weights = args_parsed.path_to_weights
 
         # ---------------------------
         # Model construction
         # ---------------------------
-        if args.model.lower() == "mlp":
+        if args_parsed.model.lower() == "mlp":
             self.model = DenoiserMLP(
                 self.dataset.image_shape,
                 self.hidden_sizes,
@@ -94,7 +92,7 @@ class Args:
                 self.dropout,
                 self.init_scheme
             )
-        elif args.model.lower() == "unet":
+        elif args_parsed.model.lower() == "unet":
             self.model = DenoiserUNet(
                 self.dataset.image_shape,
                 self.base_channels,
@@ -112,7 +110,11 @@ class Args:
                 self.dropout
             )
         else:
-            raise ValueError(f"Argument error : model {args.model} not implemented")
+            raise ValueError(f"Argument error : model {args_parsed.model} not implemented")
+
+        # Create optimizer after model is initialized
+        if self.optimizer_name.lower() == 'adam':
+            self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
 
 
 args = Args(args_parsed)
