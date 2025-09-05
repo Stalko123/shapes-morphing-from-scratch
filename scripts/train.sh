a36ca8e098e666c569c48c912cbdabe8690aaab8
Stalko123 <oussamaakar@gmail.com>
2025-09-05 15:34:53 +0200

    fixed import problems to adapt the training to the new code structure after refactoring

--- scripts/train.sh
+++ scripts/train.sh
@@ -3,15 +3,15 @@
 # Training script for DDPM shapes morphing
 # Run with: bash train.sh
 
-python trainer.py \
+python trainers/trainer.py \
     --n_epochs 100 \
     --batch_size 10 \
     --learning_rate 1e-4 \
-    --name_dataset "FashionMNIST" \
+    --dataset "MNIST" \
     --model "MLP" \
     --t_max 500 \
     --num_trials 150 \
-    --alphas "linear" \
+    --alpha_interp "linear" \
     --hidden_sizes 1024 1024 \
     --time_dim 128 \
     --activation "silu" \
