cd4d5e2ddd5feedaa95d1f9e7c61a3c612b1ab53
Baptiste Arnaudo <baptiste.arnaudo@barbeau.polytechnique.fr>
2025-09-08 11:53:03 +0200

    Added cosine alphas

--- scripts/train/train_unet_ddpm.sh
+++ scripts/train/train_unet_ddpm.sh
@@ -5,7 +5,7 @@
 
 python trainers/trainer.py \
     --t_max 1000 \
-    --n_epochs 10 \
+    --n_epochs 20 \
     --verbose \
     --model "u-net" \
     --dataset "CIFAR10" \
@@ -13,32 +13,29 @@ python trainers/trainer.py \
     --num_workers 4 \
     --validation \
     --test \
-    --use_amp \
     --val_ratio 0.05 \
     --seed 42 \
-    --num_trials 100 \
-    --alpha_min 0.95 \
-    --alpha_max 1.0 \
-    --alpha_interp "linear" \
-    --learning_rate 1e-3 \
+    --num_trials 1 \
+    --alpha_interp "cosine" \
+    --learning_rate 1e-5 \
     --dropout 0.2 \
     --optimizer_name "adam" \
     --grad_clip 0.0 \
-    --patience 10 \
+    --patience 5 \
     --activation "silu" \
-    --time_base_dim 128 \
-    --time_hidden 512 \
-    --time_output_dim 256 \
+    --time_base_dim 16 \
+    --time_hidden 32 \
+    --time_output_dim 16 \
     --init_scheme "auto" \
-    --base_channels 64 \
+    --base_channels 16 \
     --channel_mults 1,2,2 \
-    --num_res_blocks 2 \
+    --num_res_blocks 1 \
     --upsample "nearest_conv" \
-    --groups 32 \
-    --num_res_blocks_in_bottleneck 2 \
+    --groups 8 \
+    --num_res_blocks_in_bottleneck 1 \
     --norm_2d "group" \
-    --stem_kernel 5 \
-    --head_kernel 5 \
+    --stem_kernel 3 \
+    --head_kernel 3 \
     --downsample "stride" \
     --log_dir "./logs" \
     --checkpoint_dir "./checkpoints" \
